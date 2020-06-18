f770331568265c205bbdd49ac3c15166242864ef
NAGAR <sanjayrathore144@gmail.com>
2020-06-18 09:59:38 +0530

    test change

--- Frontend/src/app/website/website.component.ts
+++ Frontend/src/app/website/website.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+
+declare var $:any;
+declare var AOS:any;
 @Component({
   selector: 'app-website',
   templateUrl: './website.component.html',
@@ -10,6 +13,77 @@ export class WebsiteComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit()
+  {
+    $(function() {
+      // aos animation initialisation
+      AOS.init({
+        duration: 2000,
+        once: true
+      });
+    
+      // scroll header script here
+      window.onscroll = function() {
+        scrollHeader();
+      };
+      // Get the header
+      var header = $(".navbar-bottom");
+      var body = $("body");
+      function scrollHeader() {
+        // adding sticky class
+        if (window.pageYOffset > 130) {
+          $(header).addClass("sticky");
+        } else {
+          // removing sticky class
+          $(header).removeClass("sticky");
+        }
+      }
+    
+      // navbar toggler script
+      $(".navbar-toggler").on("click", function() {
+        $(".collapse").toggleClass("show");
+        $("body").toggleClass("layer-open");
+        // $(header).toggleClass("sticky-not");
+        $(".navbar-close").show();
+      });
+      $(".navbar-close").on("click", function() {
+        $(".collapse").toggleClass("show");
+        $(".navbar-close").hide();
+        $("body").toggleClass("layer-open");
+        // $(header).toggleClass("sticky-not");
+        $(".dark-overlay").click(function() {
+          $(".collapse").removeClass("show");
+          $("body").removeClass("layer-open");
+        });
+      });
+    
+      // $(".navbar-bottom  .navbar-nav a").on("click", function() {
+      //   $(".navbar-bottom  .navbar-nav")
+      //     .find("li.active")
+      //     .removeClass("active");
+      //   $(this)
+      //     .parent("li")
+      //     .addClass("active");
+      // });
+    
+      $("html").easeScroll({
+        frameRate: 60,
+        animationTime: 1000,
+        stepSize: 40,
+        pulseAlgorithm: 1,
+        pulseScale: 8,
+        pulseNormalize: 1,
+        accelerationDelta: 100,
+        accelerationMax: 1,
+        keyboardSupport: true,
+        arrowScroll: 50,
+        touchpadSupport: true,
+        fixedBackground: true
+      });
+    });
+    
   }
 
 }
